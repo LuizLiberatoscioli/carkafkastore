7252f921cf40fb83458fa10c27307d0744202672
Luiz <Luiz@host.docker.internal>
2023-10-28 11:07:44 -0400

    start car store kafka

---
+++ carStoreKafka/src/main/java/com/store/car/CarStoreKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.store.car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarStoreKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarStoreKafkaApplication.class, args);
+	}
+
+}
---
+++ carStoreKafka/src/test/java/com/store/car/CarStoreKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarStoreKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
