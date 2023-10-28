66c638ed970879926b72725f5a9406d090eb33c8
LuizLiberatoscioli <luizhat10@gmail.com>
2023-10-28 11:19:35 -0400

    starting structure from carStoreKafka

---
+++ carStoreKafka/src/main/java/com/store/car/entity/CarPostEntity.java
@@ -0,0 +1,14 @@
+package com.store.car.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="car_post")
+@Data
+@NoArgsConstructor
+public class CarPostEntity {
+
+}
