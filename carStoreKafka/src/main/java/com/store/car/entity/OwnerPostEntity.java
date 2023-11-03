34d44cbb0816ecfeb817cd17cbf0d8199608e187
LuizLiberatoscioli <luizhat10@gmail.com>
2023-11-03 19:15:37 -0400

    kafka configs and message

--- carStoreKafka/src/main/java/com/store/car/entity/OwnerPostEntity.java
+++ carStoreKafka/src/main/java/com/store/car/entity/OwnerPostEntity.java
@@ -28,4 +28,38 @@ public class OwnerPostEntity {
 	@Column(name = "owner_contact")
 	private String contactNumber;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getContactNumber() {
+		return contactNumber;
+	}
+
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+	
+
 }
--- carStoreKafka/src/main/java/com/store/car/service/CarPostServiceImpl.java
+++ carStoreKafka/src/main/java/com/store/car/service/CarPostServiceImpl.java
@@ -1,6 +1,7 @@
 package com.store.car.service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -23,7 +24,9 @@ public class CarPostServiceImpl implements CarPostService {
 
 	@Override
 	public void newPostDetails(CarPostDto carPostDto) {
-		// TODO Auto-generated method stub
+		 CarPostEntity carPostEntity = mapCarDtoToEntity(carPostDto);
+	        carPostRepository.save(carPostEntity);
+
 
 	}
 
@@ -74,5 +77,27 @@ public class CarPostServiceImpl implements CarPostService {
 	public void removeCarSale(Long postId) {
 		carPostRepository.deleteById(postId);
 	}
+	
+	private CarPostEntity mapCarDtoToEntity(CarPostDto carPostDTO) {
+        CarPostEntity carPostEntity = new CarPostEntity();
+
+        ownerPostRepository.findById(carPostDTO.getOwnerId()).ifPresentOrElse(item->{
+            carPostEntity.setOwnerPost(item);
+            carPostEntity.setContact(item.getContactNumber());
+        }, ()-> {
+            throw new RuntimeException();
+        });
+
+        carPostEntity.setModel(carPostDTO.getModel());
+        carPostEntity.setBrand(carPostDTO.getBrand());
+        carPostEntity.setPrice(carPostDTO.getPrice());
+        carPostEntity.setCity(carPostDTO.getCity());
+        carPostEntity.setDescription(carPostDTO.getDescription());
+        carPostEntity.setEngineVersion(carPostDTO.getEngineVersion());
+        carPostEntity.setCreatedDate(String.valueOf(new Date()));
+
+        return carPostEntity;
+    }
+
 
 }
